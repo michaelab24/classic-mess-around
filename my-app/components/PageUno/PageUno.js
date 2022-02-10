cedca44d952c08aecc7959ce9f8af8f0b90db6bf
michaelab24 <michaelbarnes459@yahoo.com>
2022-02-10 14:25:26 -0600

    a whole new world

---
+++ my-app/components/PageUno/PageUno.js
@@ -0,0 +1,18 @@
+import react from 'react';
+
+class HelloMessage extends React.Component {
+    render() {
+      return (
+        <div>
+          Hello {this.props.name}
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <HelloMessage name="Taylor" />,
+    document.getElementById('hello-example')
+  );
+
+  export default PageUno;
---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+// import PageUno from './PageUno';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi
+        </p>
+        <a
+          className="App-link"
+          href="https://en.wikipedia.org/wiki/Reefer_Madness"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Do it bitch
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
