049bea354c71d82cdde907598f227e278e131070
michaelab24 <michaelbarnes459@yahoo.com>
2022-04-16 14:27:51 -0500

    birds and pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-// import PageUno from './PageUno';
+// import logo from './logo.svg';
+// import birds from './birds.jpg';
+import PageUno from './components/PageUno';
 // import '../assets/front'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-Logo" alt="logo" />
-        <p>
-          we'll get there eventually?
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=6auk1TkGtVQ"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ice cube says you can do it yourself
-        </a>
-      </header>
+
+    <div>
+      <PageUno></PageUno>
+
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={birds} className="App-Logo" alt="logo" />
+    //     <p>
+    //       we'll get there eventually?
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://www.youtube.com/watch?v=6auk1TkGtVQ"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       ice cube says you can do it yourself
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ my-app/src/components/PageUno.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PageUno() {
+  return (
+    <div>PageUno</div>
+  )
+}
+
+//1. need photos
+//2. bring them into the component
+//3. image tags
