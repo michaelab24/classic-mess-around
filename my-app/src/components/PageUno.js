d52b5c5306b94483808985e49a69973167c64ff5
michaelab24 <michaelbarnes459@yahoo.com>
2022-04-17 12:22:57 -0500

    page uno has a start

--- my-app/src/components/PageUno.js
+++ my-app/src/components/PageUno.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
+
 export default function PageUno() {
   return (
-    <div>PageUno</div>
+    <div className="Uno">
+      <header className="UnoHeader">
+        <p className="One">
+          Bold Strategy
+        </p>
+      </header>
+   
+    </div>
   )
 }
+
 
 //1. need photos
 //2. bring them into the component
